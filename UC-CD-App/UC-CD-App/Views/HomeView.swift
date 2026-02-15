beb1611c18fc34c31141cc5544bf4aef3fb84254
yutaizawa <yutamac2512@icloud.com>
2026-02-15 10:36:17 +0900

    fix/HomeView

--- UC-CD-App/UC-CD-App/Views/HomeView.swift
+++ UC-CD-App/UC-CD-App/Views/HomeView.swift
@@ -5,6 +5,34 @@ struct HomeView: View {
     var body: some View {
         VStack {
             WeekCalendarView()
+            
+            ZStack {
+                CustomRoundedRectangleView(height: 200, title: "排便記録") {
+                    //                    ZStack() {
+                    VStack {
+                        HStack {
+                            // 左上
+                            Text("5")
+                                .font(.system(size: 50))
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 40)
+                            
+                            Button(action: {
+                                
+                            }) {
+                                Image(systemName: "plus")
+                                    .font(.system(size: 30))
+                            }
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .padding(.vertical, 25)
+                            .padding(.horizontal, 30)
+                            .buttonStyle(.plain)
+                            
+                            //                    }
+                        }
+                    }
+                }
+            }
             Spacer()
         }
         .padding(.vertical)
