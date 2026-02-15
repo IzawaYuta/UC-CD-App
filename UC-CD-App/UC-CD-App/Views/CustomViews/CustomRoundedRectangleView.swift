6060bd7a7f2f04ec1bb53b76c98c3254c6739805
yutaizawa <yutamac2512@icloud.com>
2026-02-15 10:32:12 +0900

    fix/CustomRoundedRectangleView

--- UC-CD-App/UC-CD-App/Views/CustomViews/CustomRoundedRectangleView.swift
+++ UC-CD-App/UC-CD-App/Views/CustomViews/CustomRoundedRectangleView.swift
@@ -1,25 +1,43 @@
 
 import SwiftUI
 
-struct CustomRoundedRectangleView: View {
+struct CustomRoundedRectangleView<Content: View>: View {
     
-    let hight: CGFloat
+    let height: CGFloat
+    let title: String
+    let content: Content
+    
+    init(height: CGFloat,
+         title: String,
+         @ViewBuilder content: () -> Content) {
+        self.height = height
+        self.title = title
+        self.content = content()
+    }
     
     var body: some View {
-        ZStack {
-            Color.gray.opacity(0.2)
-                .ignoresSafeArea()
-            
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.white)
-                .shadow(radius: 3, y: 5)
-                .frame(height: hight)
-                .frame(maxWidth: .infinity)
-                .padding(.horizontal, 10)
+        VStack(alignment: .leading, spacing: 2) {
+            Text(title)
+                .foregroundStyle(.gray)
+                .padding(.horizontal, 15)
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.white)
+                    .shadow(radius: 3, y: 5)
+                    .frame(height: height)
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 10)
+                
+                content
+                    .frame(height: height)  // ← 高さを制限
+                    .clipped()              // ← はみ出た部分をカット
+                    .padding(.horizontal, 10)  // ← RoundedRectangleと同じpadding
+            }
         }
+        .padding(.horizontal, 10)
     }
 }
 
 #Preview {
-    CustomRoundedRectangleView(hight: 100)
+    CustomRoundedRectangleView(height: 100, title: "記録", content: {})
 }
