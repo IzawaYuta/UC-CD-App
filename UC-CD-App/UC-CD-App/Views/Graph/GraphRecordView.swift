87b6b6327f326a69481f0e6e3119110186353663
yutaizawa <yutamac2512@icloud.com>
2026-02-22 20:48:34 +0900

    create/GraphRecordView

---
+++ UC-CD-App/UC-CD-App/Views/Graph/GraphRecordView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphRecordView.swift
+//  UC-CD-App
+//
+//  Created by Engineer MacBook Air on 2026/02/21.
+//
+
+import SwiftUI
+
+struct GraphRecordView: View {
+    var body: some View {
+        ZStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .background(Color.gray.opacity(0.2))
+    }
+}
+
+#Preview {
+    GraphRecordView()
+}
