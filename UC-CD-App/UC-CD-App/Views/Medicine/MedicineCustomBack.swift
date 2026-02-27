a5caf34f6c8abb6870bc5b35862ef87c63b677fe
yutaizawa <yutamac2512@icloud.com>
2026-02-28 06:36:36 +0900

    create/MedicineCustomBack

--- UC-CD-App/UC-CD-App/Views/Medicine/MedicineCustomBack.swift
+++ UC-CD-App/UC-CD-App/Views/Medicine/MedicineCustomBack.swift
@@ -1,18 +1,35 @@
-//
-//  MedicineCustomBack.swift
-//  UC-CD-App
-//
-//  Created by Engineer MacBook Air on 2026/02/27.
-//
 
 import SwiftUI
 
-struct MedicineCustomBack: View {
+struct MedicineCustomBack<Content: View>: View {
+    
+    let height: CGFloat
+    let content: Content
+    
+    init(height: CGFloat,
+         @ViewBuilder content: () -> Content) {
+        self.height = height
+        self.content = content()
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(Color.white)
+//                    .frame(width: geometry.size.width - 32, height: height)  // 左右16ずつ
+                
+                content
+                    .frame(width: geometry.size.width - 42)  // 32 + 左右20ずつ = 72
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .frame(height: height)
+        .padding(.horizontal, 20)
     }
 }
 
 #Preview {
-    MedicineCustomBack()
+    MedicineCustomBack(height: 50, content: {Text("あいうえお")})
+        .background(Color.gray.opacity(0.2))
 }
