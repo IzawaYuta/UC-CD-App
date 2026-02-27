9aebd68b72aec8cf22e277de2780949d14947d13
yutaizawa <yutamac2512@icloud.com>
2026-02-28 07:42:16 +0900

    fix/UC_CD_AppApp.swift

--- UC-CD-App/UC-CD-App/UC_CD_AppApp.swift
+++ UC-CD-App/UC-CD-App/UC_CD_AppApp.swift
@@ -1,40 +1,31 @@
-//
-//  UC_CD_AppApp.swift
-//  UC-CD-App
-//
-//  Created by Engineer MacBook Air on 2026/02/15.
-//
-
 import SwiftUI
 import SwiftData
 
 @main
 struct UC_CD_AppApp: App {
-//    var sharedModelContainer: ModelContainer = {
-//        let schema = Schema([
-//            Item.self,
-//        ])
-//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-//
-//        do {
-//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-//        } catch {
-//            fatalError("Could not create ModelContainer: \(error)")
-//        }
-//    }()
-
+    
+    let container: ModelContainer = {
+        try! ModelContainer(
+            for: 
+                DailyRecordModel.self,
+            DefecationRecordModel.self,
+            MealRecordModel.self,
+            MedicineModel.self,
+            MedicineUnitModel.self,
+            HospitalVisitModel.self
+            
+        )
+    }()
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
+                .task {
+                    try? MedicineUnitInitializer.insertDefaultIfNeeded(
+                        context: container.mainContext
+                    )
+                }
         }
-//        .modelContainer(sharedModelContainer)
-        .modelContainer(
-            for: [
-                DailyRecordModel.self,
-                DefecationRecordModel.self,
-                MealRecordModel.self,
-                HospitalVisitModel.self
-            ]
-        )
+        .modelContainer(container)
     }
 }
