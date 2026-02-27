7460daca8ba3bfae53e05dae3b00810af1a60924
yutaizawa <yutamac2512@icloud.com>
2026-02-28 07:33:31 +0900

    create/MedicineUnitInitializer.swift

---
+++ UC-CD-App/UC-CD-App/MedicineUnitInitializer.swift
@@ -0,0 +1,19 @@
+
+import SwiftData
+
+enum MedicineUnitInitializer {
+    
+    static func insertDefaultIfNeeded(context: ModelContext) throws {
+        
+        let descriptor = FetchDescriptor<MedicineUnitModel>()
+        let count = try context.fetchCount(descriptor)
+        
+        guard count == 0 else { return }
+        
+        context.insert(MedicineUnitModel(name: "錠", isDeletable: false))
+        context.insert(MedicineUnitModel(name: "個"))
+        context.insert(MedicineUnitModel(name: "mg"))
+        
+        try context.save()
+    }
+}
