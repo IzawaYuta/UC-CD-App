94d38ca98ada9285315d446b03edc415f5bbee48
yutaizawa <yutamac2512@icloud.com>
2026-02-28 13:58:03 +0900

    fix/初期構文変更

--- UC-CD-App/UC-CD-App/UC_CD_AppApp.swift
+++ UC-CD-App/UC-CD-App/UC_CD_AppApp.swift
@@ -4,18 +4,24 @@ import SwiftData
 @main
 struct UC_CD_AppApp: App {
     
-    let container: ModelContainer = {
-        try! ModelContainer(
-            for: 
-                DailyRecordModel.self,
+    var container: ModelContainer {
+        let schema = Schema([
+            DailyRecordModel.self,
             DefecationRecordModel.self,
             MealRecordModel.self,
             MedicineModel.self,
             MedicineUnitModel.self,
             HospitalVisitModel.self
-            
-        )
-    }()
+        ])
+        
+        let configuration = ModelConfiguration(schema: schema)
+        
+        do {
+            return try ModelContainer(for: schema, configurations: configuration)
+        } catch {
+            fatalError("Failed to create ModelContainer: \(error)")
+        }
+    }
     
     var body: some Scene {
         WindowGroup {
