4cc8db41c3c7becd6422675c7d1610b0bf73dffc
yutaizawa <yutamac2512@icloud.com>
2026-02-28 14:43:39 +0900

    create/MainTabView

---
+++ UC-CD-App/UC-CD-App/Views/Medicine/MedicineListView.swift
@@ -0,0 +1,18 @@
+//
+//  MedicineListView.swift
+//  UC-CD-App
+//
+//  Created by Engineer MacBook Air on 2026/02/28.
+//
+
+import SwiftUI
+
+struct MedicineListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MedicineListView()
+}
---
+++ UC-CD-App/UC-CD-App/Views/Tab/MainTabView.swift
@@ -0,0 +1,27 @@
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("ホーム", systemImage: "house.fill")
+                }
+            
+            MedicineInfoView()
+                .tabItem {
+                    Label("記録", systemImage: "pencil")
+                }
+            
+            MedicineListView()
+                .tabItem {
+                    Label("設定", systemImage: "gearshape.fill")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
